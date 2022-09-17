dd54225e299988ffa03828ee9ac82f9f73240567
waqas ahmad <waqaskanju@gmail.com>
2022-09-18 00:28:34 +0500

    Add migrations for tables

---
+++ db/migrate/20220917192405_add_references_to_tables.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class AddReferencesToTables < ActiveRecord::Migration[7.0]
+  def change
+    # For the post table.
+
+    add_reference :posts, :user, foreign_key: true
+
+    # For the comments table.
+
+    add_reference :comments, :user, foreign_key: true
+    add_reference :comments, :post, foreign_key: true
+
+    # For the likes table.
+
+    add_reference :likes, :user, foreign_key: true
+    add_reference :likes, :post, foreign_key: true
+
+    #  Because different in the given schema instead of
+    # author , author_id id is given.
+
+    rename_column :posts, :user_id, :author_id
+    rename_column :likes, :user_id, :author_id
+    rename_column :comments, :user_id, :author_id
+  end
+end
