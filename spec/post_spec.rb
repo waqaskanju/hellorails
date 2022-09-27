b22ad11c4b7e33b4ef67d5eade6bd8e3aeb96346
waqas ahmad <waqaskanju@gmail.com>
2022-09-27 18:39:23 +0500

    change postspec

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -5,51 +5,51 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   subject do
     Post.create(
-      author: User.create(
+      author_id: User.create(
         name: 'waqas',
         photo: 'httP://www.abc.com',
         bio: 'A bio post',
-        posts_counter: 0
+        postscounter: 0
       ),
-      title: 'Title of the post',
-      text: 'Some text of the mpost',
-      comments_counter: 0,
-      likes_counter: 0
+      Title: 'Title of the post',
+      Text: 'Some text of the mpost',
+      CommentsCounter: 0,
+      LikesCounter: 0
     )
   end
 
   # Create first test that title should not be empty.
 
   it 'Title should not be empty' do
-    subject.title = nil
+    subject.Title = nil
     expect(subject).to_not be_valid
   end
 
   # Create 2nd test that the lenght of title will not execed 250
 
   it 'title length shoud not be > 250' do
-    subject.title = 'hi' * 50
+    subject.Title = 'hi' * 50
     expect(subject).to be_valid
   end
 
   # Create 3rd test if it is greated that 250 it should fial
 
   it 'title length should not be > 250' do
-    subject.title = 'bye' * 300
+    subject.Title = 'bye' * 300
     expect(subject).to_not be_valid
   end
 
   # 4th test counter should be integer
 
   it 'Comments counter should be integer' do
-    subject.comments_counter = -5
+    subject.CommentsCounter = -5
     expect(subject).to_not be_valid
   end
 
   # fifth test like counte should be integer greater than zero.
 
   it 'Likes counter must be an integer greater than or equal to zero.' do
-    subject.likes_counter = -10
+    subject.LikesCounter = -10
     expect(subject).to_not be_valid
   end
 
